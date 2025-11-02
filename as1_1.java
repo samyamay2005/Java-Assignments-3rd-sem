187dfe42f7925f8ba04c88e5915f2d07dfc3dfef
Samyamay <samyamay2005@gmail.com>
2025-11-02 17:43:42 +0530

    initial commit

---
+++ as1_1.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class as1_1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number 1: ");
+        int num1=sc.nextInt();
+        System.out.println("Enter number 2: ");
+        int num2=sc.nextInt();
+        int sum=num1+num2;
+        System.out.println("The sum of "+num1+" and "+num2+" is: "+sum);
+        
+        sc.close();
+    }
+}
---
+++ as1_2.java
@@ -0,0 +1,5 @@
+public class as1_2 {
+    public static void main(String[] args) {
+        
+    }
+}
